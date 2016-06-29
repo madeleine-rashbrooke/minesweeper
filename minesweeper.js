2a85926816af9645ea28c4d4ac7c6e16353dbb9c
madeleine-rashbrooke <madeleine.rashbrooke@gmail.com>
2016-06-29 17:39:20 +1200

    Enable cells to be cleared

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,23 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
+
+
 function startGame () {
+  var stuff = document.getElementsByClassName('board')[0].children
+  console.log("Argh")
+  console.log(stuff)
+  addListeners(stuff)
 }
+
+function addListeners(element){
+    for (var i = 0; i<element.length; i++){
+    element[i].addEventListener('click', showCell)
+    }
+}
+
+function showCell(evt) {
+  evt.preventDefault()
+  evt.target.classList.remove('hidden')
+}
+
+startGame()
