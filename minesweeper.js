8546f373188b6ada7d3995746448a51be2145990
madeleine-rashbrooke <madeleine.rashbrooke@gmail.com>
2016-06-29 17:45:13 +1200

    Enable cells to be marked

--- minesweeper.js
+++ minesweeper.js
@@ -5,19 +5,24 @@ document.addEventListener('DOMContentLoaded', startGame)
 function startGame () {
   var stuff = document.getElementsByClassName('board')[0].children
   console.log("Argh")
-  console.log(stuff)
+  //console.log(stuff)
   addListeners(stuff)
 }
 
 function addListeners(element){
     for (var i = 0; i<element.length; i++){
     element[i].addEventListener('click', showCell)
+    element[i].addEventListener('contextmenu', markCell)
     }
 }
 
 function showCell(evt) {
-  evt.preventDefault()
   evt.target.classList.remove('hidden')
+}
+
+function markCell(evt) {
+  evt.preventDefault()
+  evt.target.classList.toggle('marked')
 }
 
 startGame()
